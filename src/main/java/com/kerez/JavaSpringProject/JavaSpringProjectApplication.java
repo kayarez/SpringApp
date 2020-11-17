66b0308e9929a65e7ab2385c2cec0ddb021c2c5b
kayarez <tansteppp@gmail.com>
2020-11-17 23:57:55 +0300

    Init

---
+++ src/main/java/com/kerez/JavaSpringProject/JavaSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.kerez.JavaSpringProject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JavaSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(JavaSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/kerez/JavaSpringProject/JavaSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kerez.JavaSpringProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
